26392330dd4e85143ff6d65f249dbfc6bdb9656b
tlange-tekconnekt <109544349+tlange-tekconnekt@users.noreply.github.com>
2022-07-18 15:23:52 -0500

    Debugging version progress

--- BGInfo/bginfo.ps1
+++ BGInfo/bginfo.ps1
@@ -1,12 +1,16 @@
 #assuming this will be only running on 64-bit computers, so for now will only work on a 64-bit implementation.
 #Reiterating above note - NOT COMPATIBLE W/ 32-bit systems (these systems should be upgraded)
-$destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup"
-$BGConfig = "$env:APPDATA\bginfo64"
+#Debugging version
+
+$date = Get-Date -UFormat "%m-%d-%y-%T"
+$logfile = "C:\bglog$date.txt"
+$destination = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup" >> $logfile
+$BGConfig = "$env:APPDATA\bginfo64" >> $logfile
 
 #Reminder to delete this line before final release
-$InstallerFolder = "C:\Users\TannerLange\Documents\GitHub\ImmyBot-Scripts\BGInfo\BGInfo-Installer-Folder"
+$InstallerFolder = "C:\Users\TannerLange\Documents\GitHub\ImmyBot-Scripts\BGInfo\BGInfo-Installer-Folder" >> $logfile
 
-Copy-Item $InstallerFolder\Bginfo64.exe $destination
-Copy-Item $InstallerFolder\Bgiconfig.bgi $BGConfig
-Invoke-Command{reg import $InstallerFolder\BGInfoDefault.reg} 
-Start-Process $destination\Bginfo64.exe -ArgumentList "$BGConfig /silent /timer0 /nolicprompt"
+Copy-Item $InstallerFolder\Bginfo64.exe $destination >> $logfile
+Copy-Item $InstallerFolder\Bgiconfig.bgi $BGConfig >> $logfile
+Invoke-Command{reg import $InstallerFolder\BGInfoDefault.reg} >> $logfile
+Start-Process $destination\Bginfo64.exe -ArgumentList "$BGConfig /silent /timer0 /nolicprompt" >> $logfile
